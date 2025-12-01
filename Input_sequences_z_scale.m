9a4403268be742a2b41f7f935e835888d9a33111
Ricky Anshuman Dash <108886729+rdashcr7@users.noreply.github.com>
2025-12-01 10:32:41 -0500

    Add files via upload

---
+++ Input_sequences_z_scale.m
@@ -0,0 +1,30 @@
+clear all; clc
+
+%% Input parameters for training network 
+
+[num1,txt1,raw1] = xlsread('Z-scale.xlsx'); % storing the z-scale values of the amino acids
+
+[num2,txt2,raw2] = xlsread("700 modified KP sequences 13 Oct.xlsx"); % storing the data of peptides' sequences and activities
+
+% total peptide count
+peptide_count = size(txt2); 
+TPC = peptide_count(1,1);
+
+%creating the input for the network in the form of z-scale values and 
+for i = 2:TPC
+    Peptide = txt2(i,1);
+    AA = char(Peptide);
+    L(i) = strlength(Peptide);
+    PL = L(i); % peptide length
+    for j=1:PL
+        for k=2:21
+            if AA(j:j) == string(txt1(k,1))
+                P1(i-1,j) = num1(k-1,1);
+                P2(i-1,j) = num1(k-1,2);
+                P3(i-1,j) = num1(k-1,3);
+            end
+        end
+    end
+end
+
+P = [P1,P2,P3];
