55ed66f65348b26334174e1fd227cbfa6867d6b5
Ricky Anshuman Dash <108886729+rdashcr7@users.noreply.github.com>
2025-12-01 10:31:05 -0500

    Update Bayesian Optimization Random Forest.py

--- Bayesian Optimization Random Forest.py
+++ Bayesian Optimization Random Forest.py
@@ -15,33 +15,27 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
 
 #%% Preparing input and output files
-in_dat = pd.read_excel(r"Modified KP sequences with z-scale.xlsx") #% Reading Input peptide sequences as z-scale numbers
-in_dat.to_csv() #% converting to CSV
+in_dat = pd.read_excel(r"Modified KP sequences with z-scale.xlsx") 
+in_dat.to_csv() 
 
-out_dat = pd.read_excel(r"700 modified KP sequences 13 Oct.xlsx") #% Reading output values from DPD simulation
-out_dat.to_csv() #% converting to CSV
-target_column = ['Mean Rg'] #% Defining output columns
+out_dat = pd.read_excel(r"700 modified KP sequences 13 Oct.xlsx") 
+out_dat.to_csv() 
+target_column = ['Mean Rg'] 
 
 #% Data splitting
 
-# df[predictors] = df[predictors]/df[predictors].max()
 X = in_dat.values #% input
 y = out_dat[target_column].values #% Output
 
-# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=6)
 
-# Set up the Random Forest model
 rf = RandomForestRegressor()
 
-# Define the search space for the hyperparameters
 search_space = {'max_depth': (1,50), 'n_estimators':(1,1000),'min_samples_split':(2,50),'min_samples_leaf':(2,50), 'max_features':[None, 'sqrt','log2'], 'bootstrap':[True, False]}
 
 
-# Perform Bayesian optimization using BayesSearchCV
 RF_model = BayesSearchCV(rf, search_space, n_iter=1000, cv=5, n_jobs=-1, random_state=6)
 
-# Fit the model (this will start the optimization process)
 RF_model.fit(X_train, y_train)
 RF_pred_train = RF_model.predict(X_train)
 RF_pred_test = RF_model.predict(X_test)
@@ -64,7 +58,6 @@ m_RF_train, b_RF_train = np.polyfit(y_train[:,0], RF_pred_train,1)
 m_RF_test, b_RF_test = np.polyfit(y_test[:,0], RF_pred_test,1)
 x_slope = np.linspace(6,13)
 
-## Plot comparing predicted training dataset output with actual training dataset output
 plt.figure(figsize=(10,5))
 plt.subplot(121)
 plt.scatter(y_train[:,0], RF_pred_train, s=15, alpha=0.6, label = 'Prediction')
@@ -77,7 +70,6 @@ plt.legend(loc=2, prop={'size':9})
 plt.grid(True)
 plt.tight_layout()
 
-## Plot comparing predicted testing dataset output with actual testing dataset output
 plt.subplot(122)
 plt.scatter(y_test[:,0], RF_pred_test, s=15, alpha=0.6, label = 'Prediction')
 plt.plot(x_slope, x_slope, 'k-', lw=3, label = 'y=x')
